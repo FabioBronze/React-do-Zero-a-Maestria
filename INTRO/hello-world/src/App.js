24bb19a9c92b40f5c2663de32dea070e0da0a966
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-02-23 19:02:38 +0000

    Hello World

---
+++ INTRO/hello-world/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <p className="paragrafo">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam
+        accusantium assumenda incidunt minima cum, omnis veritatis repellat
+        perspiciatis perferendis quia ducimus sit magnam aliquam quasi, quis
+        delectus saepe. Repellat, minima?
+      </p>
+    </div>
+  );
+}
+
+export default App;
